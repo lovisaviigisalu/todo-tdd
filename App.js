2a63b031dc9a61dc7a5d2520c804740fe0bdbb5b
Lovisa Viigisalu <l.viigisalu@gmail.com>
2024-06-16 23:17:38 +0300

    Create error handling for unit and integration tests

--- App.js
+++ App.js
@@ -7,6 +7,12 @@ mongobd.connect();
 
 app.use(express.json());
 
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        message: error.message
+    })
+})
+
 app.use("/todos", todoRoutes)
 
 app.get('/', (req, res) => {
--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,6 +1,12 @@
-const TodoModel = require('../models/todo.model')
+const TodoModel = require('../models/todo.model');
+
 const createTodo = async (req, res, next) => {
-    const createdModel = await TodoModel.create(req.body)
-    res.status(201).json(createdModel)
-}
-module.exports = {createTodo}
+    try {
+        const createdModel = await TodoModel.create(req.body);
+        res.status(201).json(createdModel);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { createTodo };
